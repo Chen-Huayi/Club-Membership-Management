b8c1c38ee3040caa38dd96f4ad5e4a9c621ab7f6
HUAYI <48390096+ChenHuayi131@users.noreply.github.com>
2022-11-10 04:04:31 -0600

    Fix user_role

--- backend/db/mongo_server.js
+++ backend/db/mongo_server.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
+// macOS
+// mongod --dbpath /usr/local/var/mongodb --logpath /usr/local/var/log/mongodb/mongo.log --fork
+
 mongoose.connect('mongodb://localhost:27017/club_member')
     .then(()=>{
-        // console.log('Database is connected')
+        console.log('Database is connected')
     })
     .catch((reason)=>{
         console.log('Fail to connect to MongoDB database')
--- frontend/src/components/authorization/AdminAuth.js
+++ frontend/src/components/authorization/AdminAuth.js
@@ -2,7 +2,7 @@ import {user_role} from '../../utils';
 import {Navigate} from 'react-router-dom'
 
 function AdminAuth({children}){
-    if (user_role==='Member Admin'){
+    if (user_role==='Membership Admin'){
         return <>{children}</>
     }else {
         return <Navigate to="/" replace/>
