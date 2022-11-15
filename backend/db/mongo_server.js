d1b5d41e2e078e2724b5326a10e3e4a679dc3fce
Chen-Huayi <1035766902@qq.com>
2022-11-15 16:21:26 -0600

    Initial default values of mongodb server.

--- backend/db/mongo_server.js
+++ backend/db/mongo_server.js
@@ -1,11 +1,29 @@
 // macOS start command:
 // mongod --dbpath /usr/local/var/mongodb --logpath /usr/local/var/log/mongodb/mongo.log --fork
 const mongoose = require('mongoose')
-const config=require('../config')
+const config = require('../config')
+const feeSchema = require("../schema/fee")
+const feeModel = mongoose.model('fees', feeSchema)
 
-mongoose.connect(config.dbServer).then(()=>{
-    // console.log('Database is connected')
-}).catch((reason)=>{
-    console.log('Fail to connect to MongoDB database')
-    console.log(reason)
-})
+mongoose.connect(config.dbServer)
+    .then(()=>{
+        console.log('Database is connected...')
+
+        feeModel.count({company: 'green_space_club'}, (err, result)=> {
+            if (err) {
+                console.log('Failed: ',err)
+            }else {
+                if (result === 0) {  // Initialize default values if this company name doesn't exist
+                    feeModel.create({company: 'green_space_club', membership_fee: 648}, (err) => {
+                        if (err)
+                            console.log('Failed: ',err)
+                        else
+                            console.log('Register company basic information successfully!')
+                    })
+                }
+            }
+        })
+    })
+    .catch((reason)=>{
+        console.log('Fail to connect to MongoDB database: ', reason)
+    })
--- backend/schema/fee.js
+++ backend/schema/fee.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const membershipFeeSchema = new Schema({
+    company: {type: String, required: true},
     membership_fee: {type: Number, required: true},
 })
 
