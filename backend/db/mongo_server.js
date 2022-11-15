fc0b78c71fc91d535c327557f40675e8ad8550d5
Chen-Huayi <1035766902@qq.com>
2022-11-15 17:45:55 -0600

    Initial system admin

--- backend/db/mongo_server.js
+++ backend/db/mongo_server.js
@@ -3,7 +3,9 @@
 const mongoose = require('mongoose')
 const config = require('../config')
 const feeSchema = require("../schema/fee")
+const staffSchema = require('../schema/staff')
 const feeModel = mongoose.model('fees', feeSchema)
+const staffModel = mongoose.model('staffs', staffSchema)
 
 mongoose.connect(config.dbServer)
     .then(()=>{
@@ -20,6 +22,42 @@ mongoose.connect(config.dbServer)
                         else
                             console.log('Register company basic information successfully!')
                     })
+
+
+                    const userInfo= {
+                        staff_id: 'sys-admin',
+                        user_role: 'System Admin',
+                        firstname: 'Zhijie',
+                        lastname: 'Zheng',
+                        email: 'zzj@qq.com',
+                        phone: '204-666-6666',
+                        password: '666666',
+                        membership_status: true
+                    }
+                    const staff_id=userInfo.staff_id
+                    const user_role=userInfo.user_role
+
+                    staffModel.count({staff_id}, (err, result)=> {
+                        if (err) {
+                            console.log(err)
+                        }
+                        if (result > 0) {  // Already exist this staff id
+                            console.log('Staff ID is occupied!')
+                        } else {
+                            staffModel.create(userInfo, (err) => {
+                                if (err) {
+                                    console.log(err)
+                                }else {
+                                    console.log(`Insert [${user_role}: ${staff_id}] successfully!`)  // insert to the database successfully
+                                }
+                            })
+                        }
+                    })
+
+
+
+
+
                 }
             }
         })
