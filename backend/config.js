eda06f0848945ac64d8402c66c14895dcc14f284
Chen-Huayi <1035766902@qq.com>
2022-11-15 21:41:10 -0600

    Deploy mongodb database on the cloud.

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,22 @@
+const db_url='ef297b1b-0ee0-4-231-b9ee:v5RzRacZA9w7VBAIlOaTkgZQkdabLiLy3iAWX1RMr5jq1WEp8N6JROR35MSKmyXddVmfyD3cqTkWTasChDdVYw%3D%3D@ef297b1b-0ee0-4-231-b9ee.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@ef297b1b-0ee0-4-231-b9ee@'
+const sysAdmin= {
+    staff_id: 'sys-admin',
+    user_role: 'System Admin',
+    firstname: '[firstname]',
+    lastname: '[lastname]',
+    email: '666666@qq.com',
+    phone: '204-666-6666',
+    password: '000000',
+    membership_status: true
+}
+
 module.exports = {
     jwtSecretKey: 'sa_nv_nei_78',
     expiresIn: '1h',
-    dbServer: 'mongodb://localhost:27017/club_member',
+    // dbServer: 'mongodb://localhost:27017/club_member',
+    dbServer: `mongodb://${db_url}`,
     PORT: 8000,
+    sysAdmin,
+    company: 'green_space_club',
+    membership_fee: 648
 }
--- backend/db/mongo_server.js
+++ backend/db/mongo_server.js
@@ -11,56 +11,44 @@ mongoose.connect(config.dbServer)
     .then(()=>{
         console.log('Database is connected...')
 
-        feeModel.count({company: 'green_space_club'}, (err, result)=> {
+        // Initialize
+        feeModel.count({company: config.company}, (err, result)=> {
             if (err) {
                 console.log('Failed: ',err)
-            }else {
-                if (result === 0) {  // Initialize default values if this company name doesn't exist
-                    feeModel.create({company: 'green_space_club', membership_fee: 648}, (err) => {
-                        if (err)
-                            console.log('Failed: ',err)
-                        else
-                            console.log('Register company basic information successfully!')
-                    })
+            }else if (result === 0) {  // Initialize default values if this company name doesn't exist
+                feeModel.create({company: config.company, membership_fee: config.membership_fee}, (err) => {
+                    if (err)
+                        console.log('Failed: ',err)
+                    else
+                        console.log('Register company basic information successfully!')
+                })
 
+                // Add system admin staff to the database
+                const userInfo=config.sysAdmin
+                const staff_id=userInfo.staff_id
+                const user_role=userInfo.user_role
 
-                    const userInfo= {
-                        staff_id: 'sys-admin',
-                        user_role: 'System Admin',
-                        firstname: 'Zhijie',
-                        lastname: 'Zheng',
-                        email: 'zzj@qq.com',
-                        phone: '204-666-6666',
-                        password: '666666',
-                        membership_status: true
+                staffModel.count({staff_id}, (err, result)=> {
+                    if (err) {
+                        console.log(err)
                     }
-                    const staff_id=userInfo.staff_id
-                    const user_role=userInfo.user_role
-
-                    staffModel.count({staff_id}, (err, result)=> {
-                        if (err) {
-                            console.log(err)
-                        }
-                        if (result > 0) {  // Already exist this staff id
-                            console.log('Staff ID is occupied!')
-                        } else {
-                            staffModel.create(userInfo, (err) => {
-                                if (err) {
-                                    console.log(err)
-                                }else {
-                                    console.log(`Insert [${user_role}: ${staff_id}] successfully!`)  // insert to the database successfully
-                                }
-                            })
-                        }
-                    })
-
-
-
-
-
-                }
+                    if (result > 0) {  // Already exist this staff id
+                        console.log('Staff ID is occupied!')
+                    } else {
+                        staffModel.create(userInfo, (err) => {
+                            if (err) {
+                                console.log(err)
+                            }else {
+                                console.log(`Insert [${user_role}: ${staff_id}] successfully!`)  // insert to the database successfully
+                            }
+                        })
+                    }
+                }) // end add system admin
             }
         })
+
+        // initialize done
+
     })
     .catch((reason)=>{
         console.log('Fail to connect to MongoDB database: ', reason)
